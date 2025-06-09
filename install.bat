8e2d62d980b5bc01f548c72cb27208100c3f4de7
vaclavmuller <v.muller@centrum.cz>
2025-06-09 08:46:57 +0200

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing required Python packages...
+python -m pip install --upgrade pip
+python -m pip install pillow
+echo.
+echo All required packages have been installed.
+pause
